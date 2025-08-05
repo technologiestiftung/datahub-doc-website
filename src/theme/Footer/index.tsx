c0e5e0e7aff30f436e861c5e18f01d3227fa5946
Lisa-Stubert <lisastubert@web.de>
2025-08-05 16:11:01 +0200

    style footer

--- src/theme/Footer/index.tsx
+++ src/theme/Footer/index.tsx
@@ -2,74 +2,74 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer className="w-full bg-white border-t-4 border-yellow-400 py-8 text-sm text-gray-700">
+    <footer className="w-full bg-white border-t-4 border-yellow-400 pb-12 pt-24 text-sm text-berlin-black">
       <div className="max-w-7xl mx-auto px-6 md:px-16">
-        <div className="grid grid-cols-12 gap-[20px]">
+        <div className="lg:grid lg:grid-cols-12 lg:gap-[20px]">
           {/* Left Side: Logos and Text */}
-          <div className="col-span-12 lg:col-span-8">
-            <div className="flex flex-wrap gap-4 items-center mb-4">
+          <div className="md:col-span-12 lg:col-span-8">
+            <div className="flex flex-col lg:flex-row flex-wrap gap-2 items-start lg:items-center mb-4">
               <img
-                src="/img/logos/datahub.svg"
+                src="/img/logos/DataHub-Logo-Prototyp-transparent-RGB.svg"
                 alt="Datahub Logo"
-                className="h-10 pr-10"
+                className="h-10 pr-10 my-2"
               />
               <img
                 src="/img/logos/tsb.svg"
                 alt="Technologiestiftung Berlin Logo"
-                className="h-10 pr-10"
+                className="h-10 pr-10 my-2"
               />
               <img
                 src="/img/logos/senatskanzlei_berlin.svg"
                 alt="Senatskanzlei Berlin Logo"
-                className="h-10"
+                className="h-10 my-2"
               />
             </div>
-            <p className="text-gray-600 text-base">
+            <p className="text-berlin-black text-base pt-6">
               Ein Projekt der Technologiestiftung Berlin. Gefördert von der
               Senatskanzlei Berlin aus den Mitteln des Landes Berlin.
             </p>
           </div>
 
           {/* Right Side: Social & Links */}
-          <div className="col-span-12 lg:col-span-4 flex flex-col items-start lg:items-end gap-4">
-            <div className="flex space-x-4">
+          <div className="col-span-12 lg:col-span-4 flex flex-col items-start lg:items-end gap-4 lg:gap-10 mt-auto pb-6 lg:pb-12">
+            <div className="flex space-x-4 pt-2">
               <a href="#">
                 <img
                   src="/img/logos/github.svg"
                   alt="GitHub"
-                  className="h-5 w-5"
+                  className="h-6 w-6"
                 />
               </a>
               <a href="#">
                 <img
                   src="/img/logos/facebook.svg"
                   alt="Facebook"
-                  className="h-5 w-5"
+                  className="h-6 w-6"
                 />
               </a>
               <a href="#">
                 <img
                   src="/img/logos/linkedin.svg"
                   alt="LinkedIn"
-                  className="h-5 w-5"
+                  className="h-6 w-6"
                 />
               </a>
               <a href="#">
                 <img
                   src="/img/logos/instagram.svg"
                   alt="Instagram"
-                  className="h-5 w-5"
+                  className="h-6 w-6"
                 />
               </a>
               <a href="#">
                 <img
                   src="/img/logos/youtube.svg"
                   alt="YouTube"
-                  className="h-5 w-5"
+                  className="h-6 w-6"
                 />
               </a>
             </div>
-            <div className="flex space-x-6 font-semibold text-black text-base">
+            <div className="flex space-x-6 font-semibold text-berlin-black text-base">
               <a href="#" className="underline underline-offset-8 decoration-2">
                 Kontakt
               </a>
