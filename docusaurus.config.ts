be454c1a6539f3f96708ea3fc7bb245910218ba0
Lisa-Stubert <lisastubert@web.de>
2025-11-05 22:29:41 +0100

    delete "bearbeiten"-feature

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -44,7 +44,7 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/technologiestiftung/datahub-doc-website',
+          editUrl: undefined, // Deaktiviert den "Edit this page"-Link global
         },
         blog: {
           showReadingTime: true,
@@ -54,7 +54,7 @@ const config: Config = {
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/technologiestiftung/datahub-doc-website',
+          editUrl: undefined, // Deaktiviert den "Edit this page"-Link global
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
@@ -81,7 +81,7 @@ const config: Config = {
       },
       items: [
         {
-          to: 'https://portal.data-hub.berlin',
+          to: 'https://portal.data-hub.berlin/',
           label: 'Portal',
           position: 'right',
           className: 'navbar__link--portal',
