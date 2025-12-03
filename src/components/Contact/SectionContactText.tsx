0c605c1641e34d750740dcd3edac2f699cfcf37f
Tim Schnettker <info@tim-schnettker.de>
2025-12-03 16:42:11 +0100

    Update SectionContactText.tsx-typo-fix

--- src/components/Contact/SectionContactText.tsx
+++ src/components/Contact/SectionContactText.tsx
@@ -7,7 +7,7 @@ export default function SectionAboutText() {
       <section className={`w-full `}>
         <div className=" max-w-7xl mx-auto md:px-16 pt-24 px-6">
           <h1 className="text-3xl md:text-6xl lg:text-6xl font-bold mb-2">
-            Nehmen Sie Kontakt auf!!!!
+            Nehmen Sie Kontakt auf!
           </h1>
           <p className="text-xl md:text-3xl pt-6 mb-0">
             Das Team der{' '}
